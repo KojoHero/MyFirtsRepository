e9c0f1380b7e74ccd54c7cc3c258688562f9129f
Assessment PC 22 <Assessment PC 22@DESKTOP-AECQN0R>
2020-01-17 16:08:17 +0000

    this just a trial

---
+++ Classcode_18122019/src/org/amalitech/classcode_18122019/Classcode_18122019.java
@@ -0,0 +1,25 @@
+package org.amalitech.classcode_18122019;
+
+public class Classcode_18122019 {
+	public static void main(String[] args) {
+		
+		
+		//boolean b_bool = true;
+		//boolean a_bool = true;
+		
+		boolean b_bool,a_bool;
+		b_bool=false;
+		a_bool =false;
+		
+		int a=1,b=1;
+		//boolean ab_bool = (a>b);
+		
+		if (b >= a) {
+			System.out.println(
+					"Hurraay..it is true."
+					);
+		}else {
+			System.out.println("Ooops! it is false");}
+	}
+
+}
