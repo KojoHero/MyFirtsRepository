2e8fdc0333bdd7773ddf1d4e3c860fa513a10ec1
KojoHero <60008545+KojoHero@users.noreply.github.com>
2020-01-26 02:35:51 -0800

    Add files via upload

---
+++ JosWheel.java
@@ -0,0 +1,47 @@
+package org.amalitech.javarecap;
+
+import java.util.Scanner;
+
+public class JosWheel  {
+static Scanner kybd_inpt = new Scanner(System.in);
+
+public static void main(String [] args) {
+System.out.println("Enter 'Start' to Begin");
+
+re_run();
+} 
+public static void re_run() {
+boolean program_running_bool = true;
+
+String code_trigger_starter_str="start";
+String code_trigger_ender_str="end";
+String kybd_inpt_str = kybd_inpt.nextLine();
+
+	if (kybd_inpt_str.equalsIgnoreCase(code_trigger_starter_str)) {
+
+			System.out.println("Enter word and it will repeat it");
+			System.out.println("");
+			System.out.println("When done, Enter 'end' to stop program running");
+
+		while  (program_running_bool == true) {
+String rpt_wrd = kybd_inpt.nextLine();
+            System.out.println(rpt_wrd);
+
+   if(rpt_wrd.equalsIgnoreCase(code_trigger_ender_str)) {
+       program_running_bool=false;
+            System.out.println("program just ended");
+            //break;
+            System.exit(0);
+
+} } }
+  if(kybd_inpt_str.equalsIgnoreCase(code_trigger_ender_str)) {
+
+System.out.println("You have not started");
+System.out.println("Enter 'Start' to begin the program");
+re_run();
+
+} else {
+System.out.println("please reset");
+re_run();
+}} } 
+
